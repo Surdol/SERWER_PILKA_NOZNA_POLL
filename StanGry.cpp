59ba8fbcd47a3ae546749ea38eb3a661dab49a33
surdol <surdol94@gmail.com>
2018-03-06 13:19:46 +0100

    lubieplacki2d

---
+++ StanGry.cpp
@@ -0,0 +1,5 @@
+//
+// Created by surdol on 06.03.18.
+//
+
+#include "StanGry.h"
---
+++ StanGry.h
@@ -0,0 +1,14 @@
+//
+// Created by surdol on 06.03.18.
+//
+
+#ifndef SERWER_PILKA_NOZNA_POLL_STANGRY_H
+#define SERWER_PILKA_NOZNA_POLL_STANGRY_H
+
+
+class StanGry {
+
+};
+
+
+#endif //SERWER_PILKA_NOZNA_POLL_STANGRY_H
